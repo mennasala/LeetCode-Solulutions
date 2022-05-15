020a6ca536dfe897f44bb1bfb2fac30227def95d
mennasala <52379016+mennasala@users.noreply.github.com>
2022-05-15 16:58:50 +0200

    Time: 154 ms (58.52%), Space: 36.5 MB (98.35%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+    int networkDelayTime(vector<vector<int>>& times, int n, int m) {
+        int d[105][105]; 
+        for(int i=0; i<105; i++){
+        for(int j=0; j<105; j++){
+            d[i][j]=1e9;
+        }
+      }
+        for(int i=0;i<times.size();i++){
+            int a,b,w;
+            a=times[i][0];
+            b=times[i][1];
+            w=times[i][2];
+            d[a][b]=w;
+        }
         
-        
-        vector<int>dis(n+1,INT_MAX);
-        dis[k]=0;
-        for(int i=1;i<n;i++)
-           {
-             for(int j=0;j<times.size();j++)
-                 {
-                   int u=times[j][0];
-                   int v=times[j][1];
-                   int w=times[j][2];
-                 
-                   if(dis[u]!=INT_MAX && dis[u]+w<dis[v])
-                   {
-                     dis[v]=w+dis[u];
-                   }
-                }
-           }
-		   
-        int ans=0;
-        for(int i=1;i<=n;i++)
-           {
-            if(dis[i]==INT_MAX) return -1;    // If any node is not reachable from k
-            ans=max(ans,dis[i]);
-           }
-        return ans;
-        
+         for (int k = 1; k <= n; k++){
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                    d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+        int mx=0;
+        for(int i=1; i<=n; i++){
+            if(i!=m){
+             mx=max(mx,d[m][i]);   
+            }
+       }
+        return (mx==1e9?-1:mx);
     }
 };
